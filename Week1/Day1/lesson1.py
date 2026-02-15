55af5185129f318dc999f6d6ddb2c206fcea15fe
cherylrosenberg <cheryllrosenberg@gmail.com>
2026-02-15 13:34:16 +0200

    Day 1 exercises xp

---
+++ Week1/Day1/lesson1.py
@@ -0,0 +1,55 @@
+# description = "strings are..."
+# description_upper = description.upper()
+# print(description_upper)
+# description_is = description.replace("are", "is")
+# print(description_is)
+# print(description[:7])
+
+# my_age = 33
+# my_age_123879 = my_age + 123879
+# print(my_age_123879)
+
+# bank_balance = '33000'
+# phone_number = 532287514
+# # print(type(bank_balance))
+# # print(type(phone_number))
+# # print(int(bank_balance))
+# print(repr(str(phone_number)))
+# print(int(phone_number))
+
+# first_name = "Cheryl"
+# last_name = "Rosenberg"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# x = 5
+# y = 10
+# z = 0
+# word1 = "hello"
+# word2 = "world"
+
+# print(bool(x < y and y > z))
+# print(bool(word1 != word2))
+# print(bool(z))
+# print(bool(word1))
+
+# name = "Alice"
+# age = "30"
+# city = "New York"
+
+# print(f"Hello, {name}! You are {age} years old and live in {city}.")
+# print(str.format("Hello, {}! You are {} years old and live in {}.", name, age, city))
+
+# age = input("What is your age?")
+# years_until_100 = 100 - int(age) 
+# print(f"You will turn 100 in {years_until_100} years.")
+
+# pi = 3.14159
+# print(f"{pi:.2f}")
+
+# name = input("What is your name?")
+# if len(name) < 5:
+#     print("Your name is short.")
+# else: 
+#     print("Your name is long.")
+
